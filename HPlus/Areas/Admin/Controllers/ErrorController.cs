89a2601bc8e144d74a53884ef42720ff2930c930
haozhiying <hzy_12@live.com>
2017-04-24 11:15:51 +0800

    修改了错误页面的 控制器

--- HPlus/Areas/Admin/Controllers/ErrorController.cs
+++ HPlus/Areas/Admin/Controllers/ErrorController.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+//
+using Aop;
+using Application;
 
 namespace HPlus.Areas.Admin.Controllers
 {
+    [@ActionFilter(false)]
+    [ExceptionFilter(false)]
     public class ErrorController : Controller
     {
         //
         // GET: /Admin/Error/
 
-        public ActionResult Index()
+        [ValidateInput(false)]
+        public ActionResult Index(CustomErrorModel cem)
         {
+            ViewData = new ViewDataDictionary<CustomErrorModel>(cem);
             return View();
         }
 
